07d33eddbfa9789a259396fb50ffbd2f821844ee
Patrick Sebastian Zimmermann <patrick@zakweb.de>
2014-09-11 01:07:37 +0200

    Rename the replacement module.

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,3 @@
-//require(['js/vendor/modernizr-2.6.2-respond-1.1.0.min.js'], function(modernizr) { }
-//require(['js/vendor/jquery-1.10.1.min.js'], function(jquery) { }
-//require(['js/vendor/bootstrap.min.js'], function(bootstrap) { }
-//requirejs(['js/vendor/modernizr-2.6.2-respond-1.1.0.min.js', '', '', 'login_popover'], function(modernizr, $, bs, login_popover) {
-//});
-
 requirejs.config({
     paths: {
         "jquery": "lib/jquery",
@@ -20,6 +14,6 @@ requirejs.config({
 require([
 'login_popover',
 'fill_resize',
-'ofbi'
+'replacement'
 ], function ($) {});
 
