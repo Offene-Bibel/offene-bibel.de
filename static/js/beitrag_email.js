2fb77f9ac825f2c9aa8aad2708792aabd1419b1f
Webserver User <u248589@dgws15s3-2-27da.ispgateway.de>
2018-11-02 18:04:37 +0100

    Add another email obfuscation helper.

--- static/js/beitrag_email.js
+++ static/js/beitrag_email.js
@@ -11,8 +11,21 @@ require ([
 
         var beitragEmail = document.getElementById('ofbi-beitrag-email');
         if (beitragEmail) {
-            beitragEmail.innerHTML = 
+            beitragEmail.innerHTML =
                 '<a class="external text" href="mailto:' + e + '?Subject=Hier%20meine%20Üb…20gerne%20einstellen%20dürft.%0A%0A%0AViele%20Grüße%0A%0AIch" rel="nofollow">'
+              + e
+              + '</a>';
+        }
+        
+        l = ['e', 'd.l', 'ebi', 'b-en', 'eff', 'o', 'mae', 'tk', 'inhcet'];
+        l.splice(6, 0, '@');
+        r = l.join("");
+        e = r.split("").reverse().join("");
+
+        var technikEmail = document.getElementById('ofbi-technik-email');
+        if (technikEmail) {
+            technikEmail.innerHTML =
+                '<a class="external text" href="mailto:' + e + '" rel="nofollow">'
               + e
               + '</a>';
         }
